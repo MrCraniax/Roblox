b8148a5182c16329f4e0ef101ff7876bf40673f8
MrCraniax <121827994+MrCraniax@users.noreply.github.com>
2023-01-02 15:16:34 -0500

    Add files via upload

---
+++ CraxHub.lua
@@ -0,0 +1,15 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("CraxHub", "LightTheme")
+
+-- MAIN --
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("HACK")
+
+
+MainSection:NewButton("Fly", "Make You Fly!", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/1vhbsj3a'))()
+end)
+
+MainSection:NewButton("Invisible", "Press E To Make You Invisible!", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/m3zMug2t'))()
+end)

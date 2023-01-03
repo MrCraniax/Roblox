aa0dd9d9e5828c99ddf07d5e57df90719f69bc83
MrCraniax <121827994+MrCraniax@users.noreply.github.com>
2023-01-02 21:56:15 -0500

    Update Engine.lua

--- Engine.lua
+++ Engine.lua
@@ -1,6 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Engine | Craniax#3936", "LightTheme")
 
+
 -- PRINCIPAL --
 local Core = Window:NewTab("Core")
 local CoreSection = Core:NewSection("【Hack】")
@@ -9,4 +10,13 @@ local CoreSection = Core:NewSection("【Hack】")
 -- HACK --
 CoreSection:NewButton("Fly", "Make You Fly!", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/Xmnf1PuY'))()
+end)
+
+
+-- HELP --
+local CoreSection = Core:NewSection("【Help】")
+
+
+-- DISCORD --
+CoreSection:NewButton("Discord.gg/Zw7JTAnu", "Join The Discord!", function()
 end)
